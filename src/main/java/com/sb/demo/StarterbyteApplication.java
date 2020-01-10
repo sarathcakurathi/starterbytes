2818b0e8d153de8972b648c5d9044a5229b4ca6d
sarathcakurathi <sarath.c.akurathi@gmail.com>
2020-01-10 07:40:16 +0530

    first commit

---
+++ src/main/java/com/sb/demo/StarterbyteApplication.java
@@ -0,0 +1,13 @@
+package com.sb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterbyteApplication.class, args);
+	}
+
+}
